4f70bbf35e2879a118e3127dbb8d98cd188ea43a
PriyaZ02 <priyadharshini@zithmi.com>
2026-05-27 12:51:26 +0530

    Radius Searching

--- src/modules/drivers/driver.service.ts
+++ src/modules/drivers/driver.service.ts
@@ -371,9 +371,9 @@ export const DriverService = {
   },
 
 
-  async findNearbyDrivers(io: Server, lng: number, lat: number, newTrip: Trip) {
+  async findNearbyDrivers(io: Server, lng: number, lat: number, newTrip: Trip, radius: number) {
     // Business Rule: We only show drivers active in the last 10 mins
-    const { drivers, searchedRadius } = await DriverRepository.findNearbyDriversExpanding(lng, lat);
+    const drivers = await DriverRepository.findNearbyDrivers(lng, lat, radius);
 
     if (!drivers || drivers.length === 0) {
       throw new Error("No drivers found in your area.");
@@ -389,7 +389,7 @@ export const DriverService = {
       await TripService.requestRideToMultipleDrivers(io, [newTrip], driversWithEta);
     }
 
-    return { drivers, searchedRadius };
+    return { drivers, searchedRadius: radius };
   },
 
   async getAvailableDrivers(lng: number, lat: number, radius: number): Promise<any[]> {
